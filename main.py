c24fc6a755d1db220c9290d1bddf76f917b046c4
SAditya10017 <AdityaShankar.Suter@outlook.com>
2026-02-23 19:46:00 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+n = int(input("te11 m3 a numb3r"))
+if n > 50:
+    print("ur number is greater than 50!")
+    if n%2 == 0:
+        print("ur number is also even!")
+    else:
+        print("ur number is also odd!")
+else:
+    print("ur number is less than 50!")
